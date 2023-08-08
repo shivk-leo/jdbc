52fcb15d971202a3063592400c59a61b6e41dbc0
shiv-leo <shiva23072000@gmail.com>
2023-08-08 21:32:51 +0530

    small change

--- jdbc_impl/src/jdbc_impl/jdbc_implement.java
+++ jdbc_impl/src/jdbc_impl/jdbc_implement.java
@@ -8,36 +8,34 @@ import java.sql.SQLException;
 public class jdbc_implement {
 
 	public static void main(String[] args) {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/your_database_name";
-        String username = "your_username";
-        String password = "your_password";
+		 String jdbcUrl = "jdbc:sqlite:C:\\Users\\sivakiran\\MySQLiteDB";
 
-        try {
-            // 1. Establish the connection
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+	        try {
+	            // 1. Establish the connection
+	            Connection connection = DriverManager.getConnection(jdbcUrl);
 
-            // 2. Create a SQL statement
-            String sql = "SELECT * FROM your_table_name";
-            PreparedStatement statement = connection.prepareStatement(sql);
+	            // 2. Create a SQL statement
+	            String sql = "SELECT * FROM Employee";
+	            PreparedStatement statement = connection.prepareStatement(sql);
 
-            // 3. Execute the query
-            ResultSet resultSet = statement.executeQuery();
+	            // 3. Execute the query
+	            ResultSet resultSet = statement.executeQuery();
 
-            // 4. Process the result
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                // Retrieve other columns as needed
-                System.out.println("ID: " + id + ", Name: " + name);
-            }
+	            // 4. Process the result
+	            while (resultSet.next()) {
+	                int id = resultSet.getInt("id");
+	                String name = resultSet.getString("name");
+	                // Retrieve other columns as needed
+	                System.out.println("ID: " + id + ", Name: " + name);
+	            }
 
-            // 5. Clean up resources
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+	            // 5. Clean up resources
+	            resultSet.close();
+	            statement.close();
+	            connection.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
     }
 }
 
